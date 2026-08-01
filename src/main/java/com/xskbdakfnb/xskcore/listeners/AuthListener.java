cb86cbdeb72ddab1bef6220112e7d523f11d45d3
katakamX <uni.katakam@gmail.com>
2026-08-01 23:54:24 +0530

    Block world interaction before login

--- src/main/java/com/xskbdakfnb/xskcore/listeners/AuthListener.java
+++ src/main/java/com/xskbdakfnb/xskcore/listeners/AuthListener.java
@@ -5,6 +5,9 @@ import com.xskbdakfnb.xskcore.managers.PlayerDataManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -16,7 +19,6 @@ public class AuthListener implements Listener {
 
         if (PlayerDataManager.isRegistered(event.getPlayer().getUniqueId())) {
 
-            // Reset login status every time the player joins
             PlayerDataManager.getPlayerData(event.getPlayer().getUniqueId()).setLoggedIn(false);
 
             event.getPlayer().sendMessage("§ePlease login using:");
@@ -53,6 +55,43 @@ public class AuthListener implements Listener {
     public void onBlockBreak(BlockBreakEvent event) {
 
         if (AuthManager.isLoggedIn(event.getPlayer().getUniqueId())) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+
+        if (AuthManager.isLoggedIn(event.getPlayer().getUniqueId())) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+
+        if (AuthManager.isLoggedIn(event.getPlayer().getUniqueId())) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+    }
+
+    @EventHandler
+    public void onItemPickup(EntityPickupItemEvent event) {
+
+        if (!(event.getEntity() instanceof org.bukkit.entity.Player player)) {
+            return;
+        }
+
+        if (AuthManager.isLoggedIn(player.getUniqueId())) {
             return;
         }
 
